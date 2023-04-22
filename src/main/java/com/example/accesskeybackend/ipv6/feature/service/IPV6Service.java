4c0cb0f3428c68dfe6039aff07cb2e7f32941c6d
Stas <s.s.kovalev.04@mail.ru>
2023-04-22 03:38:42 +0300

    ipv6 feature with DNSJava library.

---
+++ src/main/java/com/example/accesskeybackend/ipv6/feature/service/IPV6Service.java
@@ -0,0 +1,32 @@
+package com.example.accesskeybackend.ipv6.feature.service;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Service;
+
+
+import java.net.URI;
+import java.util.Arrays;
+
+
+import org.xbill.DNS.*;
+import org.xbill.DNS.Record;
+
+@Service
+public class IPV6Service {
+
+    public boolean isSupportIPV6(@NotNull String uri) {
+        try {
+            Lookup lookup = new Lookup(URI.create(uri).getHost(), Type.AAAA);
+            Record[] resultOfDnsServerLookup = lookup.run();
+            if (lookup.getResult() == Lookup.SUCCESSFUL) {
+                return Arrays
+                        .stream(resultOfDnsServerLookup)
+                        .filter(record -> record instanceof AAAARecord)
+                        .toList().size() > 0;
+            }
+        } catch (TextParseException | IllegalArgumentException e) {
+            return false;
+        }
+        return false;
+    }
+}
